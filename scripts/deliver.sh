0e7743edfb8ba571d25ba1b3cb67458c11c3ade3
Cyril Pham-le <cyril.phamle@gmail.com>
2018-01-28 20:50:06 +0100

    correct

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -5,14 +5,7 @@ rm -rf $1
 mkdir $1
 cp scripts/docker-compose.yml $1/docker-compose.yml
 cp scripts/Dockerfile $1/Dockerfile
+cp scripts/eureka-infos.yml $1/eureka-infos.yml
+cp scripts/update-dockerfile.sh $1/update-dockerfile.sh
 cp target/eureka-1.0.jar $1/eureka.jar
-cd target
-ls
 echo "End copying deployable files in app folder"
-
-#echo "Removing old files"
-#rm -f $1/docker-compose.yml
-#rm -f $1/Dockerfile
-#rm -f $1/eureka.jar
-#rm -f $1/../README.md
-#echo "End removing old files"
